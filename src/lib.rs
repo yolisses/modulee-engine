e251309c6641ccf0cede330a423b204805b8de2a
Yolisses <yolisses0@gmail.com>
2025-01-08 06:15:07 -0300

    Copy files from modulee-engine-wasm

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+mod graph;
+mod node;
+mod node_trait;
+mod node_values;
+mod nodes;
+mod utils;
---
+++ src/node.rs
@@ -0,0 +1,32 @@
+use crate::{
+    node_trait::NodeTrait,
+    node_values::NodeValues,
+    nodes::{add_node::AddNode, constant_node::ConstantNode, subtract_node::SubtractNode},
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub(crate) enum Node {
+    AddNode(AddNode),
+    SubtractNode(SubtractNode),
+    ConstantNode(ConstantNode),
+}
+
+impl NodeTrait for Node {
+    fn process(&mut self, node_values: &NodeValues) -> f64 {
+        match self {
+            Node::AddNode(add_node) => add_node.process(node_values),
+            Node::SubtractNode(subtract_node) => subtract_node.process(node_values),
+            Node::ConstantNode(constant_node) => constant_node.process(node_values),
+        }
+    }
+
+    fn get_id(&self) -> usize {
+        match self {
+            Node::AddNode(add_node) => add_node.get_id(),
+            Node::SubtractNode(subtract_node) => subtract_node.get_id(),
+            Node::ConstantNode(constant_node) => constant_node.get_id(),
+        }
+    }
+}
