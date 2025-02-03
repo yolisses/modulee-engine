456d0777d6873bb43d0bfde51680c66982e22794
Yolisses <yolisses0@gmail.com>
2025-02-03 19:16:15 -0300

    Remove get_items_by_id and items_by_id

--- src/sort/mod.rs
+++ src/sort/mod.rs
@@ -1,9 +1,7 @@
 mod get_inputs_mapping;
-mod get_items_by_id;
 mod get_topologically_sorted_node_indexes;
 pub(crate) mod has_id;
 mod inputs_mapping;
-mod items_by_id;
 mod shifts;
 pub(crate) mod sort_nodes_topologically;
 mod update_shifts;
