d56f990c98fe7c019994d497a72154e04e4f4113
Yolisses <iamurise@gmail.com>
2025-02-04 07:32:49 -0300

    Remove pitch node extras

--- src/nodes/pitch_node.rs
+++ src/nodes/pitch_node.rs
@@ -2,25 +2,21 @@ use crate::{node_trait::NodeTrait, node_values::NodeValues, sort::has_id::HasId}
 use serde::Deserialize;
 
 #[derive(Debug, PartialEq, Deserialize)]
-pub(crate) struct Extras {
-    pitch: f32,
-}
-
-#[derive(Debug, PartialEq, Deserialize)]
 pub(crate) struct PitchNode {
     id: usize,
-    extras: Extras,
+    #[serde(skip)]
+    pitch: f32,
 }
 
 impl PitchNode {
     pub(crate) fn set_pitch(&mut self, pitch: f32) {
-        self.extras.pitch = pitch;
+        self.pitch = pitch;
     }
 }
 
 impl NodeTrait for PitchNode {
     fn process(&mut self, _node_values: &NodeValues) -> f32 {
-        self.extras.pitch
+        self.pitch
     }
 
     fn get_input_ids(&self) -> Vec<usize> {
