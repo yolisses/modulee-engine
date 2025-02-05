4504facb5de8d6359a4dd2cc44940f46faa03f18
Yolisses <yolisses0@gmail.com>
2025-02-05 16:37:53 -0300

    Create GateNode

---
+++ src/nodes/gate_node.rs
@@ -0,0 +1,35 @@
+use crate::{node_trait::NodeTrait, node_values::NodeValues, sort::has_id::HasId};
+use serde::Deserialize;
+
+#[derive(Debug, PartialEq, Deserialize)]
+pub(crate) struct GateNode {
+    id: usize,
+    #[serde(skip)]
+    is_active: bool,
+}
+
+impl GateNode {
+    pub(crate) fn set_is_active(&mut self, is_active: bool) {
+        self.is_active = is_active
+    }
+}
+
+impl NodeTrait for GateNode {
+    fn process(&mut self, _node_values: &NodeValues) -> f32 {
+        if self.is_active {
+            1.
+        } else {
+            0.
+        }
+    }
+
+    fn get_input_ids(&self) -> Vec<usize> {
+        vec![]
+    }
+}
+
+impl HasId for GateNode {
+    fn get_id(&self) -> usize {
+        self.id
+    }
+}
