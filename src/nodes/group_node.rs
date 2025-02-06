6b7b99b9856a0dc269cb44ee104d8da7cc508551
Yolisses <iamurise@gmail.com>
2025-02-06 17:16:15 -0300

    Replace two TODO's by implementation

--- src/nodes/group_node.rs
+++ src/nodes/group_node.rs
@@ -1,27 +1,22 @@
 use crate::{graph::Graph, node_trait::NodeTrait, sort::has_id::HasId, values_by_id::ValuesById};
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
-pub(crate) struct InputIds {
-    frequency: usize,
-}
-
 /// Returns the phase value between 0 and 1 given a time and a frequency
 #[derive(Debug, Deserialize)]
 pub(crate) struct GroupNode {
     id: usize,
-    input_ids: InputIds,
     #[serde(skip)]
     graph: Graph,
 }
 
 impl NodeTrait for GroupNode {
     fn process(&mut self, _node_values: &ValuesById) -> f32 {
-        todo!()
+        self.graph.process();
+        self.graph.get_output_value()
     }
 
     fn get_input_ids(&self) -> Vec<usize> {
-        vec![self.input_ids.frequency]
+        vec![]
     }
 }
 
