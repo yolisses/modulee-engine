a4df6491547b5a46dd31c7d25d7902bcc15c6c04
Yolisses <yolisses0@gmail.com>
2025-02-06 18:28:07 -0300

    Fix get_items_by_id test

--- src/get_items_by_id.rs
+++ src/get_items_by_id.rs
@@ -13,7 +13,7 @@ pub(crate) fn get_items_by_id<T: HasId>(items: Vec<T>) -> HashMap<usize, T> {
 mod tests {
     use super::*;
 
-    #[derive(Debug)]
+    #[derive(Debug, PartialEq)]
     struct TestItem {
         id: usize,
         value: String,
