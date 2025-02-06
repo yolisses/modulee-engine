b0a5376026f2723ed6e5bd3faed7acd9e9f497e1
Yolisses <yolisses0@gmail.com>
2025-02-06 18:27:54 -0300

    Remove test from sort_nodes_topologically

--- src/sort/sort_nodes_topologically.rs
+++ src/sort/sort_nodes_topologically.rs
@@ -12,32 +12,3 @@ pub(crate) fn sort_nodes_topologically(nodes: &mut Vec<Node>) -> Result<(), Stri
     nodes.sort_by_key(|node| topologically_sorted_node_indexes[&node.get_id()]);
     Ok(())
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::nodes::{add_node::AddNode, constant_node::ConstantNode, output_node::OutputNode};
-
-    use super::*;
-
-    #[test]
-    fn test_sort_nodes_topologically() {
-        let mut nodes = vec![
-            Node::ConstantNode(ConstantNode::new(0, 0.0)),
-            Node::ConstantNode(ConstantNode::new(795039224, 42.0)),
-            Node::OutputNode(OutputNode::new(805174500, 904106764)),
-            Node::AddNode(AddNode::new(904106764, 0, 0)),
-        ];
-
-        sort_nodes_topologically(&mut nodes);
-
-        assert_eq!(
-            nodes,
-            vec![
-                Node::ConstantNode(ConstantNode::new(0, 0.0)),
-                Node::ConstantNode(ConstantNode::new(795039224, 42.0)),
-                Node::AddNode(AddNode::new(904106764, 0, 0)),
-                Node::OutputNode(OutputNode::new(805174500, 904106764)),
-            ]
-        );
-    }
-}
