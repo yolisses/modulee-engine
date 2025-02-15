fe504f3c53ceec8144d4c1b515420698ae3ae612
Yolisses <iamurise@gmail.com>
2025-02-15 07:46:25 -0300

    Install Criterion to use in benchmarks

--- src/lib.rs
+++ src/lib.rs
@@ -10,3 +10,20 @@ mod sample_rate;
 mod sort;
 mod values_by_id;
 mod voice;
+
+pub fn fibonacci(n: u64) -> u64 {
+    let mut a = 0;
+    let mut b = 1;
+
+    match n {
+        0 => b,
+        _ => {
+            for _ in 0..n {
+                let c = a + b;
+                a = b;
+                b = c;
+            }
+            b
+        }
+    }
+}
