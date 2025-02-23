9dbac1f8bde17b7242fb13880da5c71c94f46647
Yolisses <iamurise@gmail.com>
2025-02-23 10:17:30 -0300

    Create NoiseNode

---
+++ src/nodes/noise_node.rs
@@ -0,0 +1,26 @@
+use crate::{node_trait::NodeTrait, sort::has_id::HasId, values_by_id::ValuesById};
+use rand::{rngs::ThreadRng, Rng};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Clone)]
+pub(crate) struct NoiseNode {
+    id: usize,
+    #[serde(skip)]
+    generator: ThreadRng,
+}
+
+impl NodeTrait for NoiseNode {
+    fn process(&mut self, _node_values: &ValuesById) -> f32 {
+        self.generator.random()
+    }
+
+    fn get_input_ids(&self) -> Vec<usize> {
+        vec![]
+    }
+}
+
+impl HasId for NoiseNode {
+    fn get_id(&self) -> usize {
+        self.id
+    }
+}
