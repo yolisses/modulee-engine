8b0193201907d0326fa0857d5ad7015b1d1c16f8
Yolisses <iamurise@gmail.com>
2025-02-23 11:56:21 -0300

    Remove warn of dead code in relative_eq_array

--- src/tests/relative_eq_array.rs
+++ src/tests/relative_eq_array.rs
@@ -1,5 +1,7 @@
+#[cfg(test)]
 use assert_approx_eq::assert_approx_eq;
 
+#[cfg(test)]
 pub(crate) fn relative_eq_array(actual: Vec<f32>, expected: Vec<f32>) {
     assert_eq!(actual.len(), expected.len());
     for (actual_value, expected_value) in actual.iter().zip(expected.iter()) {
