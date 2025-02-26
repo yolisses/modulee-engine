6b1862c9e7ae71d5782fe0bfe654a9e07f92e9d9
Yolisses <yolisses0@gmail.com>
2025-02-26 17:28:27 -0300

    Organize node files in folders

---
+++ src/nodes/group/mod.rs
@@ -0,0 +1,4 @@
+pub(crate) mod group_node;
+pub(crate) mod group_voices_node;
+pub(crate) mod input_node;
+pub(crate) mod output_node;
---
+++ src/nodes/math/mod.rs
@@ -0,0 +1,4 @@
+pub(crate) mod add_node;
+pub(crate) mod divide_node;
+pub(crate) mod multiply_node;
+pub(crate) mod subtract_node;
