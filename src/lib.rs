d2ee4551984d9d7d9d7ecad932ee202ed2681fd9
Yolisses <iamurise@gmail.com>
2025-02-25 21:06:52 -0300

    Remove debug fibonacci function

--- src/lib.rs
+++ src/lib.rs
@@ -15,20 +15,3 @@ mod sort;
 mod tests;
 mod values_by_id;
 mod voice;
-
-pub fn fibonacci(n: u64) -> u64 {
-    let mut a = 0;
-    let mut b = 1;
-
-    match n {
-        0 => b,
-        _ => {
-            for _ in 0..n {
-                let c = a + b;
-                a = b;
-                b = c;
-            }
-            b
-        }
-    }
-}
