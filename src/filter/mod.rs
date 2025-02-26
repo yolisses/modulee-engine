221301a4601a1033078d078711e11ec745ab0b15
Yolisses <yolisses0@gmail.com>
2025-02-26 07:44:34 -0300

    Create low-pass filter node

--- src/filter/mod.rs
+++ src/filter/mod.rs
@@ -1,1 +1,2 @@
 pub mod high_pass;
+pub(crate) mod low_pass;
---
+++ src/nodes/low_pass_node.rs
@@ -0,0 +1,46 @@
+use crate::{
+    filter::low_pass::LowPass, node_trait::NodeTrait, sort::has_id::HasId, values_by_id::ValuesById,
+};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Clone)]
+pub(crate) struct InputIds {
+    input: usize,
+    frequency: usize,
+    resonance: usize,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub(crate) struct LowPassNode {
+    id: usize,
+    input_ids: InputIds,
+    #[serde(skip)]
+    low_pass: LowPass,
+}
+
+impl NodeTrait for LowPassNode {
+    fn process(&mut self, node_values: &ValuesById) -> f32 {
+        let input = node_values[&self.input_ids.input];
+        let frequency = node_values[&self.input_ids.frequency];
+        let resonance = node_values[&self.input_ids.resonance];
+        self.low_pass.process(input, frequency, resonance)
+    }
+
+    fn get_input_ids(&self) -> Vec<usize> {
+        vec![
+            self.input_ids.input,
+            self.input_ids.frequency,
+            self.input_ids.resonance,
+        ]
+    }
+
+    fn update(&mut self, new_node: &Self) {
+        self.input_ids = new_node.input_ids.clone();
+    }
+}
+
+impl HasId for LowPassNode {
+    fn get_id(&self) -> usize {
+        self.id
+    }
+}
