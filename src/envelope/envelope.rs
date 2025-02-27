8a45d18d4a019531bf0194f5532bafeb76259575
Yolisses <yolisses0@gmail.com>
2025-02-26 21:09:46 -0300

    Test get_is_pending for Envelope

--- src/envelope/envelope.rs
+++ src/envelope/envelope.rs
@@ -139,24 +139,30 @@ mod tests {
 
         // Idle
         relative_eq_array(get_test_values(&mut envelope, 3), vec![0., 0., 0.]);
+        assert_eq!(envelope.get_is_pending(), false);
 
         envelope.set_note_on();
 
         // Attack
         relative_eq_array(get_test_values(&mut envelope, 4), vec![0.25, 0.5, 0.75, 1.]);
+        assert_eq!(envelope.get_is_pending(), true);
 
         // Decay
         relative_eq_array(get_test_values(&mut envelope, 3), vec![0.8, 0.6, 0.4]);
+        assert_eq!(envelope.get_is_pending(), true);
 
         // Sustain
         relative_eq_array(get_test_values(&mut envelope, 7), vec![0.4; 7]);
+        assert_eq!(envelope.get_is_pending(), true);
 
         envelope.set_note_off();
 
         // Release
         relative_eq_array(get_test_values(&mut envelope, 4), vec![0.3, 0.2, 0.1, 0.]);
+        assert_eq!(envelope.get_is_pending(), true);
 
         // Idle
         relative_eq_array(get_test_values(&mut envelope, 7), vec![0.; 7]);
+        assert_eq!(envelope.get_is_pending(), false);
     }
 }
