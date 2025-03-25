34d810dafe460ab331120363ba28091f48691a80
Yolisses <yolisses0@gmail.com>
2025-03-25 07:10:34 -0300

    Remove one TODO in node_trait

--- src/node_trait.rs
+++ src/node_trait.rs
@@ -2,8 +2,6 @@ use crate::{
     has_inputs::HasInputs, has_update::HasUpdate, sort::has_id::HasId, values_by_id::ValuesById,
 };
 
-// TODO move each responsibility to a different trait, allowing easier macro
-// creation, and make NodeTrait simply a combination of them.
 pub(crate) trait NodeTrait: HasId + HasInputs + HasUpdate {
     fn process(&mut self, node_values: &ValuesById) -> f32;
 
