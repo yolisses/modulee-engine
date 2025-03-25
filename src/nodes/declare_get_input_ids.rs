5a8cda6ca74a8b320ac7d4437d935b6f86dbf6e2
Yolisses <iamurise@gmail.com>
2025-03-24 22:01:58 -0300

    Use node declaration macros in pitch node

--- src/nodes/declare_get_input_ids.rs
+++ src/nodes/declare_get_input_ids.rs
@@ -1,12 +1,12 @@
 #[macro_export]
 macro_rules! declare_get_input_ids {
-    ($node_name:ident, $($field:ident),+) => {
+    ($node_name:ident, $($field:ident),*) => {
     use crate::has_inputs::HasInputs;
 
     impl HasInputs for $node_name {
         fn get_input_ids(&self) -> Vec<usize> {
             vec![
-                $(self.input_ids.$field),+
+                $(self.input_ids.$field),*
             ]
         }
     }
