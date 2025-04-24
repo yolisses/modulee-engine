2a6b8a62459dcf88b4721776efc307b542297432
Yolisses <iamurise@gmail.com>
2025-04-24 19:37:22 -0300

    Create sort_topologically

--- src/sort/mod.rs
+++ src/sort/mod.rs
@@ -4,6 +4,7 @@ pub(crate) mod has_id;
 mod inputs_mapping;
 pub(crate) mod node_indexes;
 pub(crate) mod sort_nodes_topologically;
+mod sort_topologically;
 #[cfg(test)]
 mod tests;
 mod update_node_indexes;
