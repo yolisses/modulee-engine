88fe7b3353d906f7851865d5fbe5ffbb9190627b
Yolisses <iamurise@gmail.com>
2025-04-24 17:51:58 -0300

    Separate module_update

--- src/module/mod.rs
+++ src/module/mod.rs
@@ -1,3 +1,4 @@
 pub(crate) mod get_updated_module;
 pub(crate) mod module;
+mod module_update;
 pub(crate) mod modules_by_id;
--- src/module/module.rs
+++ src/module/module.rs
@@ -1,19 +1,14 @@
-use crate::{
-    declare_get_id, has_update::HasUpdate, node::Node, node_trait::NodeTrait,
-    set_note_trait::SetNoteTrait, sort::sort_nodes_topologically::sort_nodes_topologically,
-};
-use nohash_hasher::IntMap;
+use crate::{declare_get_id, node::Node, node_trait::NodeTrait, set_note_trait::SetNoteTrait};
 use serde::Deserialize;
-use std::error::Error;
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct Module {
     id: usize,
-    nodes: Vec<Node>,
+    pub(crate) nodes: Vec<Node>,
     // node_values is a variable used only in the process method. It is declared
     // in the struct to prevent costly allocations in each iteration
     #[serde(skip)]
-    node_values: Vec<f32>,
+    pub(crate) node_values: Vec<f32>,
 }
 
 declare_get_id! {Module}
@@ -25,22 +20,6 @@ impl PartialEq for Module {
 }
 
 impl Module {
-    // Passing node_values and input_target_ids may be a violation of the
-    // responsibility division, but improves performance
-    pub(crate) fn update_input_nodes(
-        &mut self,
-        node_values: &[f32],
-        input_target_ids: &IntMap<usize, usize>,
-    ) {
-        for node in &mut self.nodes {
-            if let Node::InputNode(input_node) = node {
-                let target_id = input_target_ids[&input_node.get_id()];
-                let value = node_values[target_id];
-                input_node.set_value(value);
-            }
-        }
-    }
-
     pub(crate) fn get_output_value(&self) -> f32 {
         for node in &self.nodes {
             if let Node::OutputNode(output_node) = node {
@@ -55,59 +34,6 @@ impl Module {
             let value = node.process(&self.node_values);
             self.node_values.insert(index, value);
         }
-    }
-
-    pub(crate) fn sort_nodes_topologically(&mut self) -> Result<(), Box<dyn Error>> {
-        sort_nodes_topologically(&mut self.nodes)?;
-        Ok(())
-    }
-
-    pub(crate) fn update(&mut self, new_module: &Module) -> Result<(), Box<dyn Error>> {
-        let new_nodes = &new_module.nodes;
-
-        // Remove nodes not present in new nodes
-        self.nodes.retain(|node| {
-            new_nodes
-                .iter()
-                .any(|new_node| new_node.get_id() == node.get_id())
-        });
-
-        for new_node in new_nodes {
-            // Update a node if present in nodes. Saves a copy of the new node
-            // otherwise
-            let id = new_node.get_id();
-            let node_option = self.nodes.iter_mut().find(|node| node.get_id() == id);
-            if let Some(node) = node_option {
-                node.update(new_node);
-            } else {
-                self.nodes.push(new_node.clone());
-            }
-        }
-
-        self.sort_nodes_topologically()?;
-
-        // Clears the node_values to prevent hard to find bugs involving deleted
-        // nodes values
-        self.node_values.clear();
-        Ok(())
-    }
-
-    pub(crate) fn update_modules_in_nodes(
-        &mut self,
-        new_modules: &IntMap<usize, Module>,
-    ) -> Result<(), Box<dyn Error>> {
-        for node in &mut self.nodes {
-            match node {
-                Node::ModuleNode(module_node) => {
-                    module_node.update_modules(new_modules)?;
-                }
-                Node::ModuleVoicesNode(module_voices_node) => {
-                    module_voices_node.update_modules(new_modules)?;
-                }
-                _ => (),
-            }
-        }
-        Ok(())
     }
 
     pub(crate) fn get_is_pending(&self) -> bool {
