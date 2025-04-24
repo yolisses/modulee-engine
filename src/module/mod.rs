26ce1137a9d8a3261de9b7c902cf71115c87dc61
Yolisses <yolisses0@gmail.com>
2025-04-24 18:54:20 -0300

    Create prepare_modules_in_nodes

--- src/module/mod.rs
+++ src/module/mod.rs
@@ -1,4 +1,3 @@
-pub(crate) mod get_updated_module;
 pub(crate) mod module;
 mod module_update;
 pub(crate) mod modules_by_id;
--- src/module/module_update.rs
+++ src/module/module_update.rs
@@ -4,7 +4,6 @@ use crate::{
     node::Node,
     sort::{has_id::HasId, sort_nodes_topologically::sort_nodes_topologically},
 };
-use nohash_hasher::IntMap;
 use std::error::Error;
 
 impl Module {
@@ -36,30 +35,20 @@ impl Module {
         Ok(())
     }
 
-    pub(crate) fn update_modules_in_nodes(
-        &mut self,
-        new_modules: &IntMap<usize, Module>,
-    ) -> Result<(), Box<dyn Error>> {
-        for node in &mut self.nodes {
-            match node {
-                Node::ModuleNode(module_node) => {
-                    module_node.update_modules(new_modules)?;
-                }
-                Node::ModuleVoicesNode(module_voices_node) => {
-                    module_voices_node.update_modules(new_modules)?;
-                }
-                _ => (),
-            }
-        }
-        Ok(())
-    }
-
-    pub(crate) fn prepare(&mut self) {
+    pub(crate) fn prepare_nodes(&mut self) {
         // TODO use result instead of unwrap
         sort_nodes_topologically(&mut self.nodes).unwrap();
 
         // TODO set input ids to indexes
+    }
 
-        // TODO find a better name for InputIds, since they can represent indexes
+    pub(crate) fn prepare_modules_in_nodes(&mut self, possible_modules: &Vec<Module>) {
+        for node in &mut self.nodes {
+            match node {
+                Node::ModuleNode(node) => node.prepare_module(possible_modules),
+                Node::ModuleVoicesNode(node) => node.prepare_module(possible_modules),
+                _ => (),
+            }
+        }
     }
 }
