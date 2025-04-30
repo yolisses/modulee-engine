3caf2768a4f1f7201c6d282a4d3ee4e913d10f0c
Yolisses <iamurise@gmail.com>
2025-04-30 15:56:02 -0300

    Create IfNode

---
+++ src/nodes/boolean/if_node.rs
@@ -0,0 +1,64 @@
+use super::get_bool_value::get_bool_value;
+use crate::node_trait::NodeTrait;
+use crate::{declare_get_id, declare_get_input_ids_and_its_getter, declare_update};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Clone)]
+pub(crate) struct IfNode {
+    id: usize,
+    input_ids: InputIds,
+}
+
+declare_get_id! {IfNode}
+declare_update! {IfNode}
+declare_get_input_ids_and_its_getter! {IfNode, condition, input1, input2}
+
+impl NodeTrait for IfNode {
+    fn process(&mut self, node_values: &[f32]) -> f32 {
+        let input1 = node_values[self.input_ids.input1];
+        let input2 = node_values[self.input_ids.input2];
+        let condition = node_values[self.input_ids.condition];
+        if get_bool_value(condition) {
+            input1
+        } else {
+            input2
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_if_node_condition_true() {
+        let mut if_node = IfNode {
+            id: 1,
+            input_ids: InputIds {
+                condition: 0,
+                input1: 1,
+                input2: 2,
+            },
+        };
+
+        let node_values = vec![1.0, 42.0, 63.0];
+        let result = if_node.process(&node_values);
+        assert_eq!(result, 42.0);
+    }
+
+    #[test]
+    fn test_if_node_condition_false() {
+        let mut if_node = IfNode {
+            id: 1,
+            input_ids: InputIds {
+                condition: 0,
+                input1: 1,
+                input2: 2,
+            },
+        };
+
+        let node_values = vec![0.0, 42.0, 63.0];
+        let result = if_node.process(&node_values);
+        assert_eq!(result, 63.0);
+    }
+}
--- src/nodes/boolean/mod.rs
+++ src/nodes/boolean/mod.rs
@@ -1,3 +1,4 @@
 pub(crate) mod and_node;
 pub(crate) mod get_bool_value;
+pub(crate) mod if_node;
 pub(crate) mod or_node;
