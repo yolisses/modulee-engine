2663997ac515f1e2f25b6e52eef7e71bad61df09
Yolisses <iamurise@gmail.com>
2025-04-30 15:43:46 -0300

    Test AddNode

--- src/nodes/math/add_node.rs
+++ src/nodes/math/add_node.rs
@@ -19,3 +19,64 @@ impl NodeTrait for AddNode {
         input1 + input2
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_node_process() {
+        let input_ids = InputIds {
+            input1: 0,
+            input2: 1,
+        };
+        let mut add_node = AddNode { id: 1, input_ids };
+
+        let node_values = vec![2., 3.];
+        let result = add_node.process(&node_values);
+
+        assert_eq!(result, 5.);
+    }
+
+    #[test]
+    fn test_add_node_with_negative_values() {
+        let input_ids = InputIds {
+            input1: 0,
+            input2: 1,
+        };
+        let mut add_node = AddNode { id: 2, input_ids };
+
+        let node_values = vec![-4., -6.];
+        let result = add_node.process(&node_values);
+
+        assert_eq!(result, -10.);
+    }
+
+    #[test]
+    fn test_add_node_with_mixed_values() {
+        let input_ids = InputIds {
+            input1: 0,
+            input2: 1,
+        };
+        let mut add_node = AddNode { id: 3, input_ids };
+
+        let node_values = vec![7.5, -2.5];
+        let result = add_node.process(&node_values);
+
+        assert_eq!(result, 5.);
+    }
+
+    #[test]
+    fn test_add_node_with_zero_values() {
+        let input_ids = InputIds {
+            input1: 0,
+            input2: 1,
+        };
+        let mut add_node = AddNode { id: 4, input_ids };
+
+        let node_values = vec![0., 0.];
+        let result = add_node.process(&node_values);
+
+        assert_eq!(result, 0.);
+    }
+}
