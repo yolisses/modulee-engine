3234ebb77a973ae6c398839ba7a4cc4d610323cc
Yolisses <iamurise@gmail.com>
2025-05-10 12:23:52 -0300

    Simplify test_update_modules

--- src/graph/graph_test.rs
+++ src/graph/graph_test.rs
@@ -9,28 +9,24 @@ mod tests {
         let mut graph = Graph::default();
 
         graph
-            .update_from_json(r#"{"main_module_id": 1, "modules": [{"id": 1, "nodes": []}]}"#)
+            .update_from_json(r#"{"main_module_id": null, "modules": [{"id": 1, "nodes": []}]}"#)
             .unwrap();
 
-        assert_eq!(graph.main_module_id, Some(1));
         assert_eq!(
             graph.modules_by_id.keys().cloned().collect::<Vec<_>>(),
             vec![1]
         );
-
         let module = &graph.modules_by_id[&1];
         assert_eq!(module.get_id(), 1);
 
         graph
-            .update_from_json(r#"{"main_module_id": 1, "modules": [{"id": 2, "nodes": []}]}"#)
+            .update_from_json(r#"{"main_module_id": null, "modules": [{"id": 2, "nodes": []}]}"#)
             .unwrap();
 
-        assert_eq!(graph.main_module_id, Some(1));
         assert_eq!(
             graph.modules_by_id.keys().cloned().collect::<Vec<_>>(),
             vec![2]
         );
-
         let module = &graph.modules_by_id[&2];
         assert_eq!(module.get_id(), 2);
     }
