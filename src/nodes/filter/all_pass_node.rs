95a2884a46b78f9f401d4a2866af0233bd5d06e0
Yolisses <iamurise@gmail.com>
2025-05-16 15:52:46 -0300

    Fix get_default_filter_wrapper for all_pass_node

--- src/nodes/filter/all_pass_node.rs
+++ src/nodes/filter/all_pass_node.rs
@@ -14,7 +14,7 @@ pub(crate) struct AllPassNode {
 }
 
 fn get_default_filter_wrapper() -> FilterWrapper {
-    FilterWrapper::new(biquad::Type::HighPass)
+    FilterWrapper::new(biquad::Type::AllPass)
 }
 
 declare_get_id! {AllPassNode}
