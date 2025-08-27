dddc5ee516fb2f08d6f8c9b1a5f8052f0218b8d9
Yolisses <iamurise@gmail.com>
2025-08-27 20:13:20 -0300

    Fix FilterWrapper and FilterWrapperWithGain nyquist_frequency

--- src/filter/filter_wrapper.rs
+++ src/filter/filter_wrapper.rs
@@ -52,5 +52,6 @@ impl FilterWrapper {
 impl SetSampleRateTrait for FilterWrapper {
     fn set_sample_rate(&mut self, sample_rate: f32) {
         self.sample_rate = sample_rate;
+        self.nyquist_frequency = sample_rate / 2.;
     }
 }
--- src/filter/filter_wrapper_with_gain.rs
+++ src/filter/filter_wrapper_with_gain.rs
@@ -55,5 +55,6 @@ impl FilterWrapperWithGain {
 impl SetSampleRateTrait for FilterWrapperWithGain {
     fn set_sample_rate(&mut self, sample_rate: f32) {
         self.sample_rate = sample_rate;
+        self.nyquist_frequency = sample_rate / 2.;
     }
 }
