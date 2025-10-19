defbe16a7a9e8a25bafeaa5c3c09b03967756b57
Yolisses <iamurise@gmail.com>
2025-10-19 10:52:34 -0300

    Fix graph test

--- src/graph/graph_test.rs
+++ src/graph/graph_test.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests {
     use crate::{sort::has_id::HasId, Graph};
-    use std::fs;
+    use std::{fs, ops::Index};
 
     #[test]
     fn test_update_modules() {
@@ -11,13 +11,29 @@ mod tests {
             .update_from_json(r#"{"main_module_id": 10, "modules": [{"id": 10, "nodes": []}]}"#)
             .unwrap();
 
-        assert_eq!(graph.main_module_instances.as_ref().unwrap().get_id(), 10);
+        assert_eq!(
+            graph
+                .main_module_instances
+                .as_ref()
+                .unwrap()
+                .index(0)
+                .get_id(),
+            10
+        );
 
         graph
             .update_from_json(r#"{"main_module_id": 20, "modules": [{"id": 10, "nodes": []}, {"id": 20, "nodes": []}]}"#)
             .unwrap();
 
-        assert_eq!(graph.main_module_instances.as_ref().unwrap().get_id(), 20);
+        assert_eq!(
+            graph
+                .main_module_instances
+                .as_ref()
+                .unwrap()
+                .index(0)
+                .get_id(),
+            20
+        );
     }
 
     #[test]
