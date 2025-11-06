5921e675bd8dbcb7606c5aa43d0034cf5d2a12ce
Yolisses <iamurise@gmail.com>
2025-11-06 07:46:54 -0300

    Format

--- src/graph/graph.rs
+++ src/graph/graph.rs
@@ -23,8 +23,8 @@ impl Graph {
     pub fn process(&mut self) {
         if let Some(main_module_instances) = &mut self.main_module_instances {
             main_module_instances[0].process(&self.empty_vector);
-            main_module_instances[1].process(&self.empty_vector);
             self.voices_cleaner.process(&mut main_module_instances[0]);
+            main_module_instances[1].process(&self.empty_vector);
             self.voices_cleaner.process(&mut main_module_instances[1]);
         }
     }
